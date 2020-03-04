7e7ddda3ad496e08711fec9fc5bbfe9e88a34e35
Sabrina Pereira <spereira@olin.edu>
2020-03-04 11:31:16 +0100

    Reformatted messages

--- sentry_release.py
+++ sentry_release.py
@@ -1,6 +1,8 @@
-import subprocess
 import os
+import subprocess
+
 import click
+
 
 @click.command()
 def sentry_release():
@@ -9,33 +11,46 @@ def sentry_release():
     org = os.getenv("SENTRY_ORG")
     project = os.getenv("SENTRY_PROJECT")
 
+    message = (
+        " The environment variable '{var_name}' is not set, and as such the release request cannot be completed.\n\t"
+        "Please set this variable and make sure it contains {message}.\n\t"
+        "Example: {example} \n"
+    )
 
     if version is None:
         raise click.ClickException(
-            " The environment variable 'APP_VERSION' is not found, and as such the release request cannot be completed.\n\t"
+            message.format(
+                var_name="APP_VERSION",
+                message="the new version tag",
+                example="APP_VERSION=v0.0.0",
+            )
         )
     if sentry_token is None:
         raise click.ClickException(
-            " The environment variable 'SENTRY_AUTH_TOKEN' is not set, and as such the release request cannot be completed.\n\t"
-            "Please set this variable and make sure it contains a valid Sentry authorization token.\n\t"
-            "Example: SENTRY_AUTH_TOKEN=dg636f1b35444f9db658d981d7e79d277898f11f388c4a18805ba67419415d78 \n"
+            message.format(
+                var_name="SENTRY_AUTH_TOKEN",
+                message="a valid Sentry authorization token",
+                example="SENTRY_AUTH_TOKEN=dg636f1b35444f9db658d981d7e79d277898f11f388c4a18805ba67419415d78",
+            )
         )
     if org is None:
         raise click.ClickException(
-            " The environment variable 'SENTRY_ORG' is not set, and as such the release request cannot be completed.\n\t"
-            "Please set this variable and make sure it contains the name of a valid Sentry organization.\n\t"
-            "Example: SENTRY_ORG=org-name \n"
+            message.format(
+                var_name="SENTRY_ORG",
+                message="the name of a valid Sentry organization",
+                example="SENTRY_ORG=org-name",
+            )
         )
     if project is None:
         raise click.ClickException(
-            " The environment variable 'SENTRY_PROJECT' is not set, and as such the release request cannot be completed.\n\t"
-            "Please set this variable and make sure it contains the name of a valid Sentry project.\n\t"
-            "Example: SENTRY_ORG=project-name \n"
+            message.format(
+                var_name="SENTRY_PROJECT",
+                message="the name of a valid Sentry project",
+                example="SENTRY_ORG=project-name",
+            )
         )
-    rc = subprocess.run('./sentry-release.sh')
+    rc = subprocess.run("./sentry-release.sh")
+
 
 if __name__ == "__main__":
     sentry_release()
-
-
-
