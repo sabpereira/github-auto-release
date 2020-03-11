953743a32f8f11ac065577d3b7245a024e67a15a
Sabrina Pereira <spereira@olin.edu>
2020-03-11 12:11:57 +0100

    Added create_commit_message_dict and new create_release_body implementations

--- release_body_generator.py
+++ release_body_generator.py
@@ -3,8 +3,11 @@ Release message body generator
 """
 
 import os
+from collections import defaultdict
 
 import github
+
+# https://docs.python.org/3/library/stdtypes.html#str.partition
 
 
 def clean_commit_message(full_commit_message, separator):
@@ -30,7 +33,7 @@ def clean_tag(full_commit_message, separator, leading_character=""):
     If there is not a prefix tag, returns "Other"
 
     """
-    tag = "Other"
+    default_tag = "Other"
     valid_tags = ["Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"]
 
     split_commit = full_commit_message.split(sep=separator, maxsplit=1)
@@ -38,27 +41,74 @@ def clean_tag(full_commit_message, separator, leading_character=""):
     possible_tag = split_commit[0].lstrip(leading_character).capitalize()
 
     if len(split_commit) > 1 and possible_tag in valid_tags:
-        tag = possible_tag
+        return possible_tag
 
-    return tag
+    return default_tag
 
 
-def create_commit_message_dict(commit_objects_list):
+def create_commit_message_dict(commit_objects_list, separator, leading_character=""):
     """
     Takes in a list of commit objects (or None if it doesn't exist)
-
-    Parses through each commit message per object and pulls out the clean tag and clean commit message.
-
     Returns a dictionary with:
         Key:   A valid tag
         Value: List of commit messages that fall under that tag
-
     Returns an empty dictionary if no commits exist
 
     """
 
+    commits_dict = defaultdict(list)
 
-def create_release_body(repo, target_commitish="master"):
+    for commit_object in commit_objects_list:
+        full_commit_message = commit_object.commit.message
+        tag = clean_tag(
+            full_commit_message=full_commit_message,
+            separator=separator,
+            leading_character=leading_character,
+        )
+        commit_message = clean_commit_message(
+            full_commit_message=full_commit_message, separator=separator
+        )
+        commits_dict[tag].append(commit_message)
+
+    return commits_dict
+
+
+def create_release_body(
+    repo, target_commitish="master", separator="] ", leading_character="["
+):
+    """
+    Takes in repo and target branch/commit for release, master is default.
+    Takes in separator and leading_character parameters based on tag formatting, [TAG] is my default.
+
+    Returns a string with the commit messages separated by tag and listed by bullet points.
+    """
+
+    latest_release_tag = repo.get_latest_release().tag_name
+    commit_objects_list = repo.compare(latest_release_tag, target_commitish).commits
+
+    commits_dict = create_commit_message_dict(
+        commit_objects_list=commit_objects_list,
+        separator=separator,
+        leading_character=leading_character,
+    )
+
+    release_body_list = []
+    bullets_string = "\n   * "
+
+    for key in commits_dict:
+        commits_dict[key].insert(0, f"{key}:")
+        commits_and_tag = bullets_string.join(commits_dict[key])
+        release_body_list.append(commits_and_tag)
+
+    release_body = "\n\n".join(release_body_list)
+
+    if not release_body:
+        release_body = "No previous commit messages to display"
+
+    return release_body
+
+
+def create_release_body_old(repo, target_commitish="master"):
     """
     Takes in repo and target branch/commit for release, master is default.
     Gathers all commit messages created since the last release to be put into message body.
@@ -82,8 +132,21 @@ def create_release_body(repo, target_commitish="master"):
 
 if __name__ == "__main__":
     github_token = os.getenv("GITHUB_TOKEN")
-    g = github.Github(github_token)
     repo = os.getenv("REPO")
-    github_repo = g.get_user().get_repo(repo)
+    g = github.Github(github_token)
+    github_repo = g.get_repo(repo)
 
-    print(create_release_body(repo))
+    latest_release_tag = github_repo.get_latest_release().tag_name
+    target_commitish = "master"
+
+    commit_objects = github_repo.compare(latest_release_tag, target_commitish).commits
+
+    commits_dict = create_commit_message_dict(
+        commit_objects_list=commit_objects, separator="] ", leading_character="["
+    )
+
+    release_body = create_release_body(github_repo)
+
+    print(release_body)
+
+    # print(create_release_body(repo))
