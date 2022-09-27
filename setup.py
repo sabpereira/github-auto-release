fd52736b6d7a67da9709523fc6f3b8bce452eb8b
Capi Etheriel <barraponto@gmail.com>
2022-09-27 10:58:56 +0200

    [Fixed][BEL-16266] fix github-auto-release install

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     name="github-auto-release",
     version="beta",
     python_requires=">=3.5, <4",
+    py_modules=[],
     install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
